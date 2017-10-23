015ca6fae8c523dda82cee8852dc082c59c9edb7
William Rudenmalm <me@whn.se>
2017-10-23 16:34:05 +0200

    Use protobuf

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,34 @@
 package scheduler
 
+import com.google.protobuf.ByteString
 import franz.ProducerBuilder
 import franz.WorkerBuilder
 import kotlinx.coroutines.experimental.runBlocking
-import java.nio.ByteBuffer
 import se.zensum.idempotenceconnector.IdempotenceStore
 import java.util.Random
 import java.util.concurrent.DelayQueue
 import java.util.concurrent.Delayed
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
+import se.zensum.scheduler_proto.Scheduler.Task
+import java.nio.charset.Charset
 
-// A message to publish
-data class ToPublish(val topic: String,
-                     val key: String?,
-                     val data: ByteBuffer)
+const val TOPIC = "scheduler"
 
 // A scheduled task
-data class ScheduledTask(val id: Long,
-                         private val time: Long,
-                         val data: ToPublish): Delayed {
+data class ScheduledTask(private val task: Task): Delayed {
     override fun compareTo(other: Delayed?): Int =
         getDelay(TimeUnit.SECONDS)
             .compareTo(other!!.getDelay(TimeUnit.SECONDS))
     override fun getDelay(unit: TimeUnit?): Long =
-        unit!!.convert(fromNow(time), TimeUnit.SECONDS)
+        unit!!.convert(fromNow(task.time), TimeUnit.SECONDS)
 
+    fun idAsString() = task.id.toString()
+    fun data() = task.kmsg
 }
 
 fun readScheduledTask(x: ByteArray): ScheduledTask =
-    ScheduledTask(1, 1, ToPublish("rhee", "normies", ByteBuffer.allocate(1)))
+    ScheduledTask(Task.parseFrom(x)!!)
 
 val idem = IdempotenceStore("scheduler")
 
@@ -49,44 +48,51 @@ class Queue {
 
 fun queueWorker(q: Queue, onItem: suspend (ScheduledTask) -> Unit) = Thread { runBlocking {
     while(!q.closed()) {
-        val p = q.take()
-
-
-        onItem(p)
+        onItem(q.take())
     }
 }}.also { it.start() }
 
 private val producer = ProducerBuilder.ofByteArray.create()
-fun publish(t: ScheduledTask) {
-    val found = idem.contains(t.id.toString())
+suspend fun publish(t: ScheduledTask) {
+    val found = idem.contains(t.idAsString())
     if (found) {
         return
     }
-    val d = t.data
-    val f = producer.sendAsync(d.topic, d.key, d.data.array())
-    f.thenAccept {
-        idem.put(t.id.toString())
-    }
+    val d = t.data()
+    producer.send(d.topic, d.key, d.body.toByteArray())
+    idem.put(t.idAsString())
 }
 
-fun runConsumer(q: Queue) =
-    WorkerBuilder.ofByteArray.handlePiped {
+suspend fun runConsumer(q: Queue) =
+    WorkerBuilder.ofByteArray
+        .subscribedTo(TOPIC)
+        .handlePiped {
         it
             .map { it.value() }
             .map { readScheduledTask(it) }
-            .advanceIf { !idem.contains(it.id.toString()) } // Ugly longs
+            .advanceIf { !idem.contains(it.idAsString()) } // Ugly longs
             .execute { q.add(it) }
             .success()
             .end()
-    }.start()
+        }.start()
 
 val rnd = Random()
-val tp = ToPublish("foo", "bar", ByteBuffer.allocate(1))
+val tp = Task.KMsg.newBuilder().apply {
+    body = ByteString.copyFrom("rhee", Charset.forName("UTF-8"))
+    key = "normies"
+    topic = "foo"
+}.build()
+
 suspend fun putExample(q: Queue) {
     val offset = rnd.nextInt(60)
-
     val time = epoch() + offset
-    q.add(ScheduledTask(rnd.nextLong(), time, tp))
+
+    val b = Task.newBuilder().apply {
+        id = rnd.nextLong()
+        setTime(time)
+        kmsg = tp
+    }.build()
+    producer.send(TOPIC, b.toByteArray())
 }
 
 fun main(args: Array<String>) = runBlocking {
